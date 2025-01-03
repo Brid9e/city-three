81240ed65991bdb3919ccb70a0f41a64a12630d2
王栋桥 <wangdongqiao1997@163.com>
2025-01-03 10:04:25 +0800

    core: change base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/city-three/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
